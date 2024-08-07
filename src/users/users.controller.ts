21912ac2f2250e5fbb1fdbd6ab5e8501a1f046fe
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2024-08-07 00:28:13 -0300

    feat: create routes in user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -17,18 +26,23 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @Get('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.usersService.login(loginUserDto);
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,86 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginUserDto } from './dto/login-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.prismaService.user.create({
+      data: {
+        email: createUserDto.email,
+        start_time_service: createUserDto.start_time_service,
+        end_time_service: createUserDto.end_time_service,
+        name: createUserDto.name,
+        password: this.generateHash(createUserDto.password),
+        phone: createUserDto.phone,
+        id_establishment: createUserDto.id_establishment,
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prismaService.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.prismaService.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prismaService.user.update({
+      data: {
+        start_time_service: updateUserDto.start_time_service,
+        end_time_service: updateUserDto.end_time_service,
+        phone: updateUserDto.phone,
+        id_establishment: updateUserDto.id_establishment,
+      },
+      where: {
+        id: id,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.prismaService.user.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  findByEmail(email: string) {
+    return this.prismaService.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+  }
+
+  async login(LoginUserDto: LoginUserDto) {
+    const user = await this.findByEmail(LoginUserDto.email);
+
+    const isPassword = await bcrypt.compare(
+      LoginUserDto.password,
+      user.password,
+    );
+
+    if (!user || !isPassword) {
+      throw new Error('Email ou senha inválidos.');
+    }
+
+    return user;
+  }
+
+  generateHash(password: string) {
+    return bcrypt.hashSync(password, 10);
   }
 }
