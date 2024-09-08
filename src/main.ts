25438d9902601e7a519e2f63d33ca09fbdc7e3fe
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2024-09-08 00:30:53 -0300

    feat: config cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // Permite apenas requests de um domínio específico
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Accept', // Cabeçalhos permitidos
+    credentials: true, // Permite o envio de cookies
+  });
   // await app.listen(3000);
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
